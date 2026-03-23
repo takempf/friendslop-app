4b0ebfb194dde86a4bb53a18109ba500eae62278
Timothy Kempf <tim@kempf.dev>
2026-03-23 12:55:57 -0700

    prevent shadows from being cut off

--- src/components/SchoolEnvironment.tsx
+++ src/components/SchoolEnvironment.tsx
@@ -20,7 +20,20 @@ export function SchoolEnvironment() {
     <group>
       {/* Lights */}
       <ambientLight intensity={0.5} />
-      <directionalLight position={[10, 20, 10]} intensity={1.5} castShadow />
+      <directionalLight 
+        position={[20, 40, 20]} 
+        intensity={1.5} 
+        castShadow 
+        shadow-mapSize-width={2048}
+        shadow-mapSize-height={2048}
+        shadow-camera-near={0.1}
+        shadow-camera-far={200}
+        shadow-camera-left={-40}
+        shadow-camera-right={40}
+        shadow-camera-top={40}
+        shadow-camera-bottom={-40}
+        shadow-bias={-0.0001}
+      />
 
       {/* --- Gymnasium --- */}
       {/* Floor 20x20 */}
